062772d1791ef21c073b8bace24f390dec2c2f6f
elt0nxale <eltontay1@gmail.com>
2025-01-19 18:23:34 +0800

    chore: add github token

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,7 @@
 module.exports = {
     ci: {
         collect: {
-            staticDistDir: './build',
+            staticDistDir: '.next',
         },
         upload: {
             target: 'temporary-public-storage',
