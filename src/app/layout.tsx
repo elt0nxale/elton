c8f390500aba70555a50c6c6502768626827da87
elt0nxale <eltontay1@gmail.com>
2025-01-19 20:06:00 +0800

    feat: add speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import { ThemeProvider } from '@/contexts/ThemeContext';
 import LayoutContent from '@/components/LayoutContent';
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
         <body className={`bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100 ${inter.className}`}>
           <LayoutContent>{children}</LayoutContent>
           <Analytics/>
+          <SpeedInsights />
         </body>
       </ThemeProvider>
     </html>
